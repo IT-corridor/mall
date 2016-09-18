2697fdd36bff5983bfcc20b0565103465ae52f53
niklak2 <morgenpurple@gmail.com>
2016-09-18 11:45:00 +0300

    add production config

---
+++ data/deploy/pro/gunicorn/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NAME=mall_api
+BASEDIR=/home/django/
+DJANGODIR="${BASEDIR}/mall/back"
+ENVDIR="${BASEDIR}/venv"
+SOCKFILE="/tmp/${NAME}.sock"
+NUM_WORKERS=3
+DJANGO_WSGI_MODULE="back.wsgi"
+GUNICORN=GUNICORN=${ENVDIR}/bin/gunicorn
+
+cd $DJANGODIR
+source "${ENVDIR}/bin/activate"
+
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+exec $GUNICORN "${DJANGO_WSGI_MODULE}:application" \
+  --env DJANGO_SETTINGS_MODULE=settings.local \
+  --workers $NUM_WORKERS \
+  --bind "unix:${SOCKFILE}" \
